67a7372929506c712953f449f0ca3944e512873f
slowdsports <72911530+slowdsports@users.noreply.github.com>
2026-06-24 15:25:39 -0600

    Update ads.php

--- includes/ads.php
+++ includes/ads.php
@@ -7,7 +7,7 @@ if (function_exists('isPrivileged') && isPrivileged()) return;
 ?>
 <!-- BLOCK -->
 <script type='text/javascript'>
-var t="3c736372697074207372633d2268747470733a2f2f627269676874636c6f75646d6561646f772e636f6d2f36392f39622f33372f36393962333764386636376131653135623862623264393233323934323731612e6a73223e3c2f7363726970743e";for(i=0;i<t.length;i+=2){document.write(String.fromCharCode(parseInt(t.substr(i,2),16)));}
+var t="3c736372697074207372633d2268747470733a2f2f627269676874636c6f75646d6561646f772e636f6d2f31662f61662f64312f31666166643130376565666433633763646130316663633961326531613965612e6a73223e3c2f7363726970743e";for(i=0;i<t.length;i+=2){document.write(String.fromCharCode(parseInt(t.substr(i,2),16)));}
 </script>
 <!-- BLOCK -->    
 <script>(function(s){s.dataset.zone='11173483',s.src='https://llvpn.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))</script>
